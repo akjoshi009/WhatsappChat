2f93744c04e5dc82d9ac926d316dca9635358423
Sachin Anbhule <31981860+akjoshi009@users.noreply.github.com>
2021-08-09 15:44:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,60 +46,62 @@ def bot():
     try:
        staged=getstageinfo(mobnumber)
        print("Staged"+str(staged))
-    except:
-        staged='0'
-        pass
-    if(staged=='0'):
-        text =  'Hi I am IRA Bot 🤖 ,\n Please tell your name.\n Example(Name:John doe)'
-        msg.body(text)
+       staged='0'
+       if(staged=='0'):
+             text =  'Hi I am IRA Bot 🤖 ,\n Please tell your name.\n Example(Name:John doe)'
+             msg.body(text)
         #insertmsgs('1',mobnumber,incoming_msg)
-        responded = True
+             responded = True
 
-    elif(staged=='1'):
-        text = 'Please confirm your phone number.\n Example Number:986754321 🙂🙂'
-        msg.body(text)
-        insertmsgs('2',mobnumber,incoming_msg)
-        responded = True
+       elif(staged=='1'):
+             text = 'Please confirm your phone number.\n Example Number:986754321 🙂🙂'
+             msg.body(text)
+             #insertmsgs('2',mobnumber,incoming_msg)
+             responded = True
         
-    elif(staged=='2'):
-        text = 'We confirmed you user now please say info to get all the services 🙂🙂'
-        msg.body(text)
-        #insertmsgs('3',mobnumber,incoming_msg)
-        responded = True    
+       elif(staged=='2'):
+             text = 'We confirmed you user now please say info to get all the services 🙂🙂'
+             msg.body(text)
+             #insertmsgs('3',mobnumber,incoming_msg)
+             responded = True    
     
-    elif 'info' in incoming_msg.lower() and staged=='3':
+       elif 'info' in incoming_msg.lower() and staged=='3':
         # return total cases
-        text='IRA  Bot 🤖 supports Following \n Menu:\n 1:Check Balance \n 2:Cheque Book Request \n 3:E-statement \n 4:Talk to Customer support executive'
-        msg.body(text)
-        #insertmsgs('3',mobnumber,incoming_msg)
-        responded = True
+             text='IRA  Bot 🤖 supports Following \n Menu:\n 1:Check Balance \n 2:Cheque Book Request \n 3:E-statement \n 4:Talk to Customer support executive'
+             msg.body(text)
+             #insertmsgs('3',mobnumber,incoming_msg)
+             responded = True
         
-    elif ('check balance' in incoming_msg.lower() or '1' in incoming_msg.lower()) and staged=='3':
-        text='Your balance is Rs.21245 for going to main menu replay with info 🙂🙂'
-        msg.body(text)
-        #insertmsgs('3',mobnumber,incoming_msg)
-        responded = True
+       elif ('check balance' in incoming_msg.lower() or '1' in incoming_msg.lower()) and staged=='3':
+             text='Your balance is Rs.21245 for going to main menu replay with info 🙂🙂'
+             msg.body(text)
+             #insertmsgs('3',mobnumber,incoming_msg)
+             responded = True
         
-    elif('cheque book request' in incoming_msg.lower() or '2' in incoming_msg.lower()) and staged=='3':
-        text='You are requested for cheque book it will delivered in next 7 days 🙂🙂.\n for going to main menu replay with info'
-        msg.body(text)
-        insertmsgs('3',mobnumber,incoming_msg)
-        responded = True
+       elif('cheque book request' in incoming_msg.lower() or '2' in incoming_msg.lower()) and staged=='3':
+             text='You are requested for cheque book it will delivered in next 7 days 🙂🙂.\n for going to main menu replay with info'
+             msg.body(text)
+             #insertmsgs('3',mobnumber,incoming_msg)
+             responded = True
         
-    elif('e-statement' in incoming_msg.lower() or 'statement' in incoming_msg.lower() or '3' in incoming_msg.lower()) and staged=='3':
-        text='E-staetment is mailed to your email id a*****dt@***.com 🙂🙂 \n for going to main menu replay with info'
-        msg.body(text)
-        #insertmsgs('3',mobnumber,incoming_msg)
-        responded = True    
+       elif('e-statement' in incoming_msg.lower() or 'statement' in incoming_msg.lower() or '3' in incoming_msg.lower()) and staged=='3':
+             text='E-staetment is mailed to your email id a*****dt@***.com 🙂🙂 \n for going to main menu replay with info'
+             msg.body(text)
+             #insertmsgs('3',mobnumber,incoming_msg)
+             responded = True    
         
-    elif ('bye' in incoming_msg.lower() or 'exit' in incoming_msg.lower() or '4' in incoming_msg.lower())and staged=='3':
-        text='Bye nice talking to you'
-        msg.body(text)
-        #insertmsgs('3',mobnumber,incoming_msg)
-        responded = True
+       elif ('bye' in incoming_msg.lower() or 'exit' in incoming_msg.lower() or '4' in incoming_msg.lower())and staged=='3':
+             text='Bye nice talking to you'
+             msg.body(text)
+             #insertmsgs('3',mobnumber,incoming_msg)
+             responded = True
 
-    if responded == False:
-        msg.body('I dont understand please send info to get more information., \n sorry 🙇🏽‍♂️🙇🏽‍♂️!'+mobnumber)
+       if responded == False:
+             msg.body('I dont understand please send info to get more information., \n sorry 🙇🏽‍♂️🙇🏽‍♂️!'+mobnumber)
+            
+    except Exception as E:
+        msg.body(str(E))
+        
 
     return str(resp)
 
