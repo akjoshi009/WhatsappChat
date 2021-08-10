08a908c9d35afaf3a2ebc96d8e5097a6c3718428
Sachin Anbhule <31981860+akjoshi009@users.noreply.github.com>
2021-08-10 11:13:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug  9 14:18:08 2021
-
-@author: user
-"""
-
 from flask import Flask, request
 import requests
 import json
@@ -12,32 +5,9 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-staged='0'
-Get_Stage = "http://kms.1point1.in/voicebot/api/v1/getstage?id="
-
-addstageinfo="http://kms.1point1.in/voicebot/api/v1/createconversation?"
-
-def getstageinfo(number):
-    r = requests.get(url = Get_Stage+number)
-    if(len(r)>0):
-        return r['stage']
-    else:
-        return '0'
-    
-def insertmsgs(stage,number,msg):
-    fnurl=addstageinfo+'stage='+stage+'&number='+number+'&msg='+msg
-    r = requests.get(url = fnurl)
-    print(r)
-    
-    
-
 @app.route("/")
 def hello():
-    url2='http://kms.1point1.in/voicebot/api/v1/getstage?id=12456456'
-    dt = requests.get(url = url2)
-    data = dt.json()
-    #print(data)
-    return "Hello World!"+str(data)
+    return "Hello World!"
 
 @app.route('/bot', methods=['POST'])
 def bot():
@@ -47,65 +17,51 @@ def bot():
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
-    try:
-       staged=getstageinfo(mobnumber)
-       print("Staged"+str(staged))
-       staged='0'
-       if(staged=='0'):
-             text =  'Hi I am IRA Bot 🤖 ,\n Please tell your name.\n Example(Name:John doe)'
-             msg.body(text)
-        #insertmsgs('1',mobnumber,incoming_msg)
-             responded = True
-
-       elif(staged=='1'):
-             text = 'Please confirm your phone number.\n Example Number:986754321 🙂🙂'
-             msg.body(text)
-             #insertmsgs('2',mobnumber,incoming_msg)
-             responded = True
-        
-       elif(staged=='2'):
-             text = 'We confirmed you user now please say info to get all the services 🙂🙂'
-             msg.body(text)
-             #insertmsgs('3',mobnumber,incoming_msg)
-             responded = True    
     
-       elif 'info' in incoming_msg.lower() and staged=='3':
-        # return total cases
-             text='IRA  Bot 🤖 supports Following \n Menu:\n 1:Check Balance \n 2:Cheque Book Request \n 3:E-statement \n 4:Talk to Customer support executive'
-             msg.body(text)
-             #insertmsgs('3',mobnumber,incoming_msg)
-             responded = True
-        
-       elif ('check balance' in incoming_msg.lower() or '1' in incoming_msg.lower()) and staged=='3':
-             text='Your balance is Rs.21245 for going to main menu replay with info 🙂🙂'
-             msg.body(text)
-             #insertmsgs('3',mobnumber,incoming_msg)
-             responded = True
-        
-       elif('cheque book request' in incoming_msg.lower() or '2' in incoming_msg.lower()) and staged=='3':
-             text='You are requested for cheque book it will delivered in next 7 days 🙂🙂.\n for going to main menu replay with info'
-             msg.body(text)
-             #insertmsgs('3',mobnumber,incoming_msg)
-             responded = True
-        
-       elif('e-statement' in incoming_msg.lower() or 'statement' in incoming_msg.lower() or '3' in incoming_msg.lower()) and staged=='3':
-             text='E-staetment is mailed to your email id a*****dt@***.com 🙂🙂 \n for going to main menu replay with info'
-             msg.body(text)
-             #insertmsgs('3',mobnumber,incoming_msg)
-             responded = True    
-        
-       elif ('bye' in incoming_msg.lower() or 'exit' in incoming_msg.lower() or '4' in incoming_msg.lower())and staged=='3':
-             text='Bye nice talking to you'
-             msg.body(text)
-             #insertmsgs('3',mobnumber,incoming_msg)
-             responded = True
+    if 'hi' == incoming_msg.lower() or 'hey' == incoming_msg.lower() or 'hello' == incoming_msg.lower():
+        text =  'Hi I am IRA Bot 🤖 ,\n Please tell your name.\n Example(Name:John doe)'
+        msg.body(text)
+        responded = True
 
-       if responded == False:
-             msg.body('I dont understand please send info to get more information., \n sorry 🙇🏽‍♂️🙇🏽‍♂️!'+mobnumber)
-            
-    except Exception as E:
-        msg.body(str(E))
+    elif 'name' in incoming_msg.lower():
+        text = 'Please confirm your phone number.\n Example Number:986754321 🙂🙂'
+        msg.body(text)
+        responded = True
         
+    elif 'number' in incoming_msg.lower():
+        text = 'We confirmed you user now please say info to get all the services 🙂🙂'
+        msg.body(text)
+        responded = True
+    
+    
+    elif 'info' in incoming_msg.lower():
+        # return total cases
+        text='IRA  Bot 🤖 supports Following \n Menu:\n 1:Check Balance \n 2:Cheque Book Request \n 3:E-statement \n 4:Exit'
+        msg.body(text)
+        responded = True
+        
+    elif 'check balance' in incoming_msg.lower() or '1' in incoming_msg.lower():
+        text='Your balance is Rs.21245 for going to main menu replay with info 🙂🙂'
+        msg.body(text)
+        responded = True
+        
+    elif 'cheque book request' in incoming_msg.lower() or '2' in incoming_msg.lower():
+        text='You are requested for cheque book it will delivered in next 7 days 🙂🙂.\n for going to main menu replay with info'
+        msg.body(text)
+        responded = True
+        
+    elif 'e-statement' in incoming_msg.lower() or 'statement' in incoming_msg.lower() or '3' in incoming_msg.lower():
+        text='E-staetment is mailed to your email id a*****dt@***.com 🙂🙂 \n for going to main menu replay with info'
+        msg.body(text)
+        responded = True    
+        
+    elif 'bye' in incoming_msg.lower() or 'exit' in incoming_msg.lower() or '4' in incoming_msg.lower():
+        text='Bye nice talking to you'
+        msg.body(text)
+        responded = True
+
+    if responded == False:
+        msg.body('I dont understand please send info to get more information., \n sorry 🙇🏽‍♂️🙇🏽‍♂️!'+mobnumber)
 
     return str(resp)
 
